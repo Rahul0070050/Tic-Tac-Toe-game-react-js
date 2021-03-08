9a6820efcce53dfc8bdcb224ede0537ee21bab13
Rahul <rahulor2000@gmail.com>
2021-03-08 12:39:44 +0530

    funished

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { useGlobalContest } from './Context';
 import './style.css';
 
 function App() {
-  const { start, won, finish, startGame, handleRestart } = useGlobalContest();
+  const { start, won, started, startGame, handleRestart } = useGlobalContest();
   return (
     <section className="container" >
       <header>
         <h1>Tic Tac Toe</h1>
       </header>
       <main>
-        {/* {won} */}
+        {won && <h1>{won} won</h1>}
         {start ? < Playground /> : <div className="start"><h1>Start the game</h1><br /> <button onClick={startGame}>Start</button></div>}
-        {finish && <button onClick={handleRestart}>restart</button>}
+        {started && <button onClick={handleRestart}>restart</button>}
       </main>
     </section >
   );
